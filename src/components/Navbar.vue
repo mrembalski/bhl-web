40bad6b8463fb322e5a69489b33b8bcc12a74906
Paweł Kowalski <paw.kow.2k@gmail.com>
2020-11-06 19:39:33 +0100

    Basic setup with placeholder code

---
+++ src/components/Navbar.vue
@@ -0,0 +1,44 @@
+<template>
+    <div>
+  <b-navbar toggleable="lg" type="dark" variant="dark">
+    <b-navbar-brand href="#">0xD859</b-navbar-brand>
+
+    <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+    <b-collapse id="nav-collapse" is-nav>
+      <b-navbar-nav>
+      <b-nav-item to="/">Home</b-nav-item>
+      <b-nav-item to="/about">About</b-nav-item>
+      </b-navbar-nav>
+
+      <b-navbar-nav class="ml-auto">
+        <b-nav-item-dropdown text="Grupa" right>
+          <b-dropdown-item v-for="lang in langs" :key="langs" href="#">{{ lang }}</b-dropdown-item>
+        </b-nav-item-dropdown>
+
+        <b-nav-item-dropdown right>
+          <template #button-content>
+            {{ username }}
+          </template>
+          <b-dropdown-item href="#">Profil</b-dropdown-item>
+          <b-dropdown-item href="#">Ustawienia</b-dropdown-item>
+          <b-dropdown-item href="#">Wyloguj</b-dropdown-item>
+        </b-nav-item-dropdown>
+      </b-navbar-nav>
+    </b-collapse>
+  </b-navbar>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'Navbar',
+  props: ['username'],
+  data() {
+      return {
+          langs: [this.username, 'test', '123']
+      };
+   }
+
+}
+</script>
